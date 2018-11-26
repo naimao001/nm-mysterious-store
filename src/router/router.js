b0a9d9ea743c6bca9da9c03f63b9197d6085ce58
naimao <1024175936@qq.com>
2018-11-26 16:50:02 +0800

    购物车完成

--- src/router/router.js
+++ src/router/router.js
@@ -3,18 +3,20 @@ import VueRouter from "vue-router"
 import Index from "../components/Index.vue"
 import Detail from "../components/Detail.vue"
 import ShopCar from "../components/ShopCar.vue"
+import Order from "../components/Order.vue"
 Vue.use(VueRouter)
 
-let routes = [ 
-{ path: "/", name: "Index", component: Index },
-{path:"/detail/:artId",name:"Detail",component: Detail},
-{path:"/shopcar",name:"ShopCar",component: ShopCar}
+let routes = [
+  { path: "/", name: "Index", component: Index },
+  { path: "/detail/:artId", name: "Detail", component: Detail },
+  { path: "/shopcar", name: "ShopCar", component: ShopCar },
+  { path: "/order", name: "Order", component: Order }
 ]
 
 const router = new VueRouter({
   routes,
-   //每次跳到新路由 就会在页面顶部开始
-   scrollBehavior () {
+  //每次跳到新路由 就会在页面顶部开始
+  scrollBehavior() {
     return { x: 0, y: 0 }
   }
 })
